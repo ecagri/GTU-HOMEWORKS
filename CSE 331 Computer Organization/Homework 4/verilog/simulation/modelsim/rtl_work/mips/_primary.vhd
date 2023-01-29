20cf37adb9e29e39effffe81903127d4d0be14ba
ecagri <c.cayci21@gmail.com>
2023-01-30 00:10:09 +0300

    Unnecessary folders are removed

--- CSE 331 Computer Organization/Homework 4/verilog/simulation/modelsim/rtl_work/mips/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mips is
-    port(
-        clock           : in     vl_logic;
-        clock2          : in     vl_logic
-    );
-end mips;
--- CSE 331 Computer Organization/Homework 4/verilog/simulation/modelsim/work/datapath_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity datapath_tb is
-end datapath_tb;
